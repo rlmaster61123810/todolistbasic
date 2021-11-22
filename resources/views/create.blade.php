92d4090ca561ff5caa2da805e1471938d4655e33
The new begin <61123810@g.cmru.ac.th>
2021-11-22 09:45:15 +0700

    home

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,9 +9,6 @@
         <label for="title">Title</label>
         <input type="text" class="form-control" id="title" name="title">
     </div>
-    <div class="form-group">
-        <label for="description">Description</label>
-        <textarea class="form-control" id="description" name="description" rows="3"></textarea>
-    </div>
+
     <button type="submit" class="btn btn-primary">Submit</button>
 @endsection
